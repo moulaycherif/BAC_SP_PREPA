0f767e3db616b4c2ad88a466a50811ba08e2061b
moulaycherif <moulay2011@gmail.com>
2026-07-20 16:08:04 +0100

    Premier commit

--- qcm-frontend/src/config.ts
+++ qcm-frontend/src/config.ts
@@ -1,2 +1,5 @@
-export const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+// On récupère l'URL (depuis Vercel ou en local)
+const rawUrl = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
+
+// 🪄 LA MAGIE ICI : On supprime automatiquement le slash à la toute fin s'il existe !
+export const API_BASE_URL = rawUrl.replace(/\/$/, "");
